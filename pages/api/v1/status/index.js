3cfe1ec01e592176adcbb337ef7035d5e4a42fd1
Matheus Heleno Lopes <matheuslopesheleno@gmail.com>
2025-08-06 23:06:32 +0000

    finish /api/v1/status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Código 200, OK" });
+  const dbName = process.env.POSTGRES_DB;
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConectionsValue = parseInt(
+    dbMaxConectionsResult.rows[0].max_connections,
+  );
+
+  const dbUsedConectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbUsedConectionsValues = dbUsedConectionsResult.rows[0].count;
+
+  console.log(dbUsedConectionsValues);
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: dbMaxConectionsValue,
+        used_connections: dbUsedConectionsValues,
+      },
+    },
+  });
 }
 
 export default status;
