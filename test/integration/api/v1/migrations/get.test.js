b0fbe370cad767d3e9eaab0795cdde438597eff0
Matheus Heleno Lopes <matheuslopesheleno@gmail.com>
2025-09-29 23:20:35 +0000

    fix /migrations endpoint connection bug

--- test/integration/api/v1/migrations/get.test.js
+++ test/integration/api/v1/migrations/get.test.js
@@ -8,7 +8,7 @@ async function cleanDataBase() {
 
 test("GET top /api/v1/migrations should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
-  console.log("Fazendo um commit para testar o preview.");
+
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
