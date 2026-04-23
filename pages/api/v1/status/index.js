ba5368935b581aa866fd07b76db341a00893d89f
Matheus Heleno Lopes <matheuslopesheleno@gmail.com>
2026-04-23 21:37:36 +0000

    refactor: use next-connect in /migrations and /status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,45 +1,39 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalServerError } from "infra/errors";
+import controller from "infra/controller";
 
-async function status(request, response) {
-  try {
-    const dbName = process.env.POSTGRES_DB;
-    const updatedAt = new Date().toISOString();
+const router = createRouter();
 
-    const dbVersionResult = await database.query("SHOW server_version;");
-    const dbVersionValue = dbVersionResult.rows[0].server_version;
+router.get(getHandler);
 
-    const dbMaxConectionsResult = await database.query("SHOW max_connections;");
-    const dbMaxConectionsValue = parseInt(
-      dbMaxConectionsResult.rows[0].max_connections,
-    );
+export default router.handler(controller.errorHandlers);
 
-    const dbUsedConectionsResult = await database.query({
-      text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
-      values: [dbName],
-    });
-    const dbUsedConectionsValues = dbUsedConectionsResult.rows[0].count;
+async function getHandler(request, response) {
+  const dbName = process.env.POSTGRES_DB;
+  const updatedAt = new Date().toISOString();
 
-    response.status(200).json({
-      updated_at: updatedAt,
-      dependecies: {
-        database: {
-          version: dbVersionValue,
-          max_connections: dbMaxConectionsValue,
-          used_connections: dbUsedConectionsValues,
-        },
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const dbVersionValue = dbVersionResult.rows[0].server_version;
+
+  const dbMaxConectionsResult = await database.query("SHOW max_connections;");
+  const dbMaxConectionsValue = parseInt(
+    dbMaxConectionsResult.rows[0].max_connections,
+  );
+
+  const dbUsedConectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const dbUsedConectionsValues = dbUsedConectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependecies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: dbMaxConectionsValue,
+        used_connections: dbUsedConectionsValues,
       },
-    });
-  } catch (error) {
-    const publicErrorObject = new InternalServerError({
-      cause: error,
-    });
-
-    console.log("\n Error dentro do catch do Controller");
-    console.log(publicErrorObject);
-
-    response.status(500).json(publicErrorObject);
-  }
+    },
+  });
 }
-
-export default status;
